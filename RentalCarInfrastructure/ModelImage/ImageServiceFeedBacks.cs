276cdceaa7944b1276e5ec17ef1f0313b81f1173
adesopequazeem@gmail.com <adesopequazeem@gmail.com>
2022-05-11 21:45:05 +0100

    Did some refactoring of codes in ImageService

---
+++ RentalCarInfrastructure/ModelImage/ImageServiceFeedBacks.cs
@@ -0,0 +1,12 @@
+﻿namespace RentalCarInfrastructure.ModelImage
+{
+    public class ImageServiceFeedBacks
+    {
+        public const string photoSettings = "PhotoSettings:Size";
+        public const string sizeExceeded = "File size exceeded";
+        public const string photoFormat = "PhotoSettings:Formats";
+        public const string unsupportedFormat = "File format not supported";
+        public const string crop = "crop";
+        public const string gravity = "gravity";
+    }
+}
