6a0b703cd8dcaabd04264f25a4ca9b17813cf486
Mac <mac@Macs-MacBook-Pro.local>
2022-05-13 22:04:51 +0100

    modify user dto

--- RentalCarCore/Dtos/UserResponseDto.cs
+++ RentalCarCore/Dtos/UserResponseDto.cs
@@ -11,5 +11,7 @@ namespace RentalCarCore.Dtos
         public string Id { get; set; }
         public string Token { get; set; }
         public string RefreshToken { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
     }
 }
